192cac7d4643fe2c8e9e6c35cc297e19cc4695bd
Monica Hung <monica.hung@mycase.com>
2018-12-05 17:25:24 -0800

    Day 3 Part 2

--- day3/day3.rb
+++ day3/day3.rb
@@ -14,6 +14,8 @@ def main(file_name)
   end
   # Solution Part 1
   puts overlapping(claims)
+  # Solution Part 2
+  puts my_overlapping(claims)
 end
 
 # xdist = claim[1]
@@ -38,6 +40,38 @@ def overlapping(claims)
     end
   end
   overlap
+end
+
+# claimnumber = claim[0]
+# xdist = claim[1]
+# ydist = claim[2]
+# xlength = claim[3]
+# ylength = claim[4]
+def my_overlapping(claims)
+  # Make the fabric
+  x = Array.new(1000)
+  (0..(x.length - 1)).each do |row|
+    x[row] = Array.new(1000, 0)
+  end
+  untouched = Array.new(claims.length, true)
+  # Apply each claim and count the overlaps
+  claims.each do |claim|
+    (0..(claim[3] - 1)).each do |xind|
+      (0..(claim[4] - 1)).each do |yind|
+        square = x[claim[1] + xind][claim[2] + yind]
+        # It's free real estate, take it as your own
+        if square.zero?
+          x[claim[1] + xind][claim[2] + yind] = claim[0]
+        else
+          untouched[square-1] = false
+          untouched[claim[0]-1]=false
+        end
+      end
+    end
+  end
+  num = untouched.find_index(true)
+  return num + 1 unless num.nil?
+  'NOBODY IS PERFECT'
 end
 
 main('input_pt1.txt')
